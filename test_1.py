d15e116183742687d0c85135f292ee8338ce78cb
Ricarvy <597928240@qq.com>
2017-12-31 11:46:07 +0800

    2017-12-31

--- test_1.py
+++ test_1.py
@@ -1,10 +1,28 @@
 import cv2
+import matplotlib.pyplot as plt
 print(cv2.__version__)
-img=cv2.imread(filename='data/opencv_test_img.jpg',flags=cv2.IMREAD_GRAYSCALE)
+
+### Read an image
+img=cv2.imread(filename='data/ali.jpg')
 
 ### Resize the window
-### 
-cv2.namedWindow('img',flags=cv2.WINDOW_NORMAL)
+# cv2.namedWindow('img',flags=cv2.WINDOW_NORMAL)
 cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+### show the image using plt
+# plt.imshow(img)
+# plt.xticks([]),plt.yticks([])
+# plt.show()
+
+### receive the keyboard event
+k=cv2.waitKey(0)
+print(k)
+if k == ord('s'):
+    ### Write an image
+    ### save the handled image to working directory
+    cv2.imwrite('img_gray.png', img=img)
+    cv2.destroyAllWindows()
+if k == 27:
+    cv2.destroyAllWindows()
+
+
