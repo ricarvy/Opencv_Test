f75ae0933eb8c9b769fb4b076424ae956a1614bf
Ricarvy <597928240@qq.com>
2017-12-31 11:17:05 +0800

    second commition

--- test_1.py
+++ test_1.py
@@ -3,6 +3,7 @@ print(cv2.__version__)
 img=cv2.imread(filename='data/opencv_test_img.jpg',flags=cv2.IMREAD_GRAYSCALE)
 
 ### Resize the window
+### 
 cv2.namedWindow('img',flags=cv2.WINDOW_NORMAL)
 cv2.imshow('img',img)
 cv2.waitKey(0)
